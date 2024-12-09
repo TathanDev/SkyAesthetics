007589532888f58cf1debc7bbaa1335cce57c2ea
TATHAN <costa.tathan@gmail.com>
2024-12-09 18:42:25 +0100

    Re-Add Rain particle removal

---
+++ common/src/main/java/fr/tathan/sky_aesthetics/mixin/client/RemoveSnowAndRainMixin.java
@@ -0,0 +1,41 @@
+package fr.tathan.sky_aesthetics.mixin.client;
+
+import fr.tathan.sky_aesthetics.client.skies.utils.SkyHelper;
+import net.minecraft.client.Camera;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.WeatherEffectRenderer;
+import net.minecraft.server.level.ParticleStatus;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(WeatherEffectRenderer.class)
+public class RemoveSnowAndRainMixin {
+
+    @Inject(
+            method = "render(Lnet/minecraft/world/level/Level;Lnet/minecraft/client/renderer/LightTexture;IFLnet/minecraft/world/phys/Vec3;)V",
+            at = @At(
+                    value = "HEAD"
+            ), cancellable = true)
+    private void renderCustomSkyboxes(Level level, LightTexture lightTexture, int i, float f, Vec3 vec3, CallbackInfo ci) {
+        if(level instanceof ClientLevel clientLevel) {
+            SkyHelper.canRenderSky(clientLevel, (planetSky -> {
+                if(!planetSky.getProperties().rain()) ci.cancel();
+            }));
+        }
+    }
+    @Inject(
+            method = "tickRainParticles",
+            at = @At(
+                    value = "HEAD"
+            ), cancellable = true)
+    public void tickRainParticles(ClientLevel clientLevel, Camera camera, int i, ParticleStatus particleStatus, CallbackInfo ci) {
+        SkyHelper.canRenderSky(clientLevel, (planetSky -> {
+            if(!planetSky.getProperties().rain()) ci.cancel();
+        }));
+    }
+}
