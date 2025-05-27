6163b1f57eb482120736562ae7a0a3936bf6af62
TATHAN <costa.tathan@gmail.com>
2025-05-27 21:52:27 +0200

    Better config system

--- common/src/main/java/fr/tathan/sky_aesthetics/config/SkyConfig.java
+++ common/src/main/java/fr/tathan/sky_aesthetics/config/SkyConfig.java
@@ -1,23 +1,16 @@
 package fr.tathan.sky_aesthetics.config;
 
-import com.google.gson.JsonElement;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.JsonOps;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 
-import java.util.List;
+public class SkyConfig {
 
-public record SkyConfig(List<String> disabledSkies) {
 
-    public static final Codec<SkyConfig> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
-            Codec.STRING.listOf().fieldOf("disabledSkies").forGetter(SkyConfig::disabledSkies)
-    ).apply(instance, SkyConfig::new));
+    public String[] disabledSkies = new String[]{
+            "bad_sky:sky_to_disable"
+    };
 
-    public JsonElement toJson() {
-        return CODEC
-                .encodeStart(JsonOps.INSTANCE, this)
-                .result()
-                .orElseThrow(() -> new IllegalStateException("Failed to encode to JSON"));
-    }
+    public String[] disabledDimensions = new String[]{
+            "bad_mod:bad_dimension"
+    };
+
 
 }
