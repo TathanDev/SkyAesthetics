1fd67440d8bdcc8e6e85ca80bc0f75c47ba2b0ce
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-12-06 06:47:06 -0500

    fix fabric

--- fabric/src/main/java/fr/tathan/sky_aesthetics/fabric/SkyAestheticsClientFabric.java
+++ fabric/src/main/java/fr/tathan/sky_aesthetics/fabric/SkyAestheticsClientFabric.java
@@ -2,6 +2,7 @@ package fr.tathan.sky_aesthetics.fabric;
 
 import fr.tathan.SkyAesthetics;
 import fr.tathan.exoconfig.platform.PlatformHelper;
+import fr.tathan.exoconfig.platform.PlatformHelperClient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.IdentifiableResourceReloadListener;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
@@ -20,7 +21,7 @@ public final class SkyAestheticsClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
-        PlatformHelper.registerConfigScreen(SkyAesthetics.MODID, SkyAesthetics.CONFIG);
+        PlatformHelperClient.registerConfigScreen(SkyAesthetics.MODID, SkyAesthetics.CONFIG);
 
         onAddReloadListener();
     }
