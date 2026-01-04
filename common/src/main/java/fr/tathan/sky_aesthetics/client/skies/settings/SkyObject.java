6edc18333e008acecfe0a237e9817227f57dbc09
TATHAN <costa.tathan@gmail.com>
2026-01-04 16:53:00 +0100

    Sky Object logic

--- common/src/main/java/fr/tathan/sky_aesthetics/client/skies/settings/SkyObject.java
+++ common/src/main/java/fr/tathan/sky_aesthetics/client/skies/settings/SkyObject.java
@@ -99,16 +99,18 @@ public record SkyObject(Identifier texture, boolean blend, float size, Vector3f 
 
 
 
-    public void renderObject(float alpha, PoseStack poseStack, TextureAtlas celestial) {
+    public void renderObject(float alpha, PoseStack poseStack, TextureAtlas celestial, float sunAngle, float moonAngle) {
         RenderSystem.AutoStorageIndexBuffer quadIndices = RenderSystem.getSequentialBuffer(VertexFormat.Mode.QUADS);
 
         GpuBuffer objectBuffer = this.buildSkyObject(celestial);
 
         Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
         matrix4fStack.pushMatrix();
+
         matrix4fStack.mul(poseStack.last().pose());
         matrix4fStack.translate(0.0F, 100.0F, 0.0F);
-        matrix4fStack.scale(30.0F, 1.0F, 30.0F);
+        matrix4fStack.scale(this.size, 1.0F, this.size);
+
         GpuBufferSlice gpuBufferSlice = RenderSystem.getDynamicUniforms().writeTransform(matrix4fStack, new Vector4f(1.0F, 1.0F, 1.0F, alpha), new Vector3f(), new Matrix4f());
         GpuTextureView gpuTextureView = Minecraft.getInstance().getMainRenderTarget().getColorTextureView();
         GpuTextureView gpuTextureView2 = Minecraft.getInstance().getMainRenderTarget().getDepthTextureView();
--- common/src/main/java/fr/tathan/sky_aesthetics/mixin/client/TestRendering.java
+++ common/src/main/java/fr/tathan/sky_aesthetics/mixin/client/TestRendering.java
@@ -1,11 +1,16 @@
 package fr.tathan.sky_aesthetics.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import fr.tathan.sky_aesthetics.client.skies.settings.SkyObject;
 import net.minecraft.client.renderer.SkyRenderer;
+import net.minecraft.client.renderer.state.SkyRenderState;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.level.MoonPhase;
+import org.joml.Vector3f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,14 +25,20 @@ public class TestRendering {
     @Shadow
     private TextureAtlas celestialsAtlas;
 
-
     @Inject(method = "renderSunMoonAndStars", at = @At("TAIL"), cancellable = true)
-    public void renderSunMoonAndStars(PoseStack poseStack, float f, float g, float h, MoonPhase moonPhase, float i, float j, CallbackInfo ci) {
+    public void renderSunMoonAndStars(PoseStack poseStack, float sunAngle, float moonAngle, float starAngle, MoonPhase moonPhase, float rainBrightness, float starBrightness, CallbackInfo ci) {
         poseStack.pushPose();
+        poseStack.mulPose(Axis.XP.rotation(sunAngle));
         SkyObject object = new SkyObject(Identifier.withDefaultNamespace("sun"),
-                false, 1, null,
-                null, 1, null);
-        object.renderObject(1, poseStack, celestialsAtlas);
+                false, 70, new Vector3f(0),
+                new Vector3f(0), 1, "DAY");
+
+
+        object.setObjectRotation(poseStack);
+        object.setObjectPosition(poseStack, sunAngle);
+
+        object.renderObject(1, poseStack, celestialsAtlas, sunAngle, moonAngle);
         poseStack.popPose();
     }
+
 }
